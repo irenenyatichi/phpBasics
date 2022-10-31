f67884c40b69d372a7b3b185f77cd10c00cbf95d
irenenyatichi <irenenyatichik@gmail.com>
2022-10-31 16:22:43 +0300

    Data Types

--- Magic Constants.php
+++ Magic Constants.php
@@ -1,14 +1,18 @@
-MAGIC CONSTANTS
-<!--These are predefined PHP constants.-->
-<!--They get changed on the basis of use.-->
-<!--They start with the double underscore (__) and ends with the double underscore.-->
-<!--They are similar to other predefined constants but they change their values with the context, they are called magic constants.-->
-<!---->
-<!--        1. __LINE__-->
-<!--Returns the current line number of the file at the time of file compilation-->
 <?php
+namespace GeeksforGeeks;
+class Company {
+    public function gfg() {
+        return __namespace__;
+    }}
+$obj = new Company();
+echo $obj->gfg();
+?>
+
+    <?php
 echo "The Line number : ". __LINE__;
 ?>
+        1. __LINE__-->
+<!--Returns the current line number of the file at the time of file compilation-->
 
         2.__FILE__
 Returns the full path of the executed file with the name of the file.
@@ -44,31 +48,23 @@ echo "Integer Maximum Value : ".PHP_INT_MAX;
 echo "Integer Minimum Value : ".PHP_INT_MIN;
 ?>
 
-        6. __namespace__
-Returns the current namespace where the magic constant is included
-<?php
-namespace GeeksforGeeks;
-class Company {
-    public function gfg() {
-        return __namespace__;
-    }}
-$obj = new Company();
-echo $obj->gfg();
-?>
+//trait Mango{
+//    function gfg(){
+//        echo __trait__;
+//    }
+//}
+//class Fruit{
+//    use Mango;
+//}
+//$a = new Company;
+//$a->gfg();
+//?>
 
-        7. __trait__
-Returns the trait name.
-<?php
-trait Mango{
-    function gfg(){
-        echo __trait__;
-    }
-}
-class Fruit{
-    use Mango;
-}
-$a = new Company;
-$a->gfg();
-?>
-
+<!--These are predefined PHP constants.-->
+<!--They get changed on the basis of use.-->
+<!--They start with the double underscore (__) and ends with the double underscore.-->
+<!--They are similar to other predefined constants but they change their values with the context, they are called magic constants.-->
+<!---->
+<!--        1. __LINE__-->
+<!--Returns the current line number of the file at the time of file compilation-->
 
